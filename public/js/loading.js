05c7273d190bd24f67c2be47bf3771f1995586f6
antoniussinaga-dev <antoniussinaga.dev@gmail.com>
2023-10-28 02:15:19 +0700

    many redirecting

--- public/js/loading.js
+++ public/js/loading.js
@@ -2,8 +2,10 @@
 function LoadPage() {
     if(window.innerWidth < 700 && window.location.pathname != "/m") {
         window.location.replace("/m")
+        return
     } else if (window.innerWidth > 700 && window.location.pathname != "/") {
         window.location.replace("/")
+        return
     }
 }
 
